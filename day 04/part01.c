09dd6cbfcedcbeffa76b565cc2f910f15f73efa9
Zisis Avouris <zisisavouris@gmail.com>
2021-12-04 19:15:49 +0200

    day 04 completed

---
+++ day 04/part01.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdbool.h>
+
+#define INPUT_LEN 100
+#define NUM_OF_BOARDS 100
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+
+u8 order[INPUT_LEN];
+u8 *buffer;
+u8 winning_num;
+u8 winner_board;
+bool *checks;
+bool winner_flag = false;
+
+void find_num(u16 start, u16 end, u8 key);
+bool check_for_bingo(u16 start, u16 end, u8 key, u8 board);
+
+u16 sum_winner_board(u8 board);
+
+int main(void)
+{
+    FILE *file;
+    u16 i, bingo;
+	buffer = (u8*) malloc((5*5*NUM_OF_BOARDS)*sizeof(u8));
+	checks = (bool*) malloc((5*5*NUM_OF_BOARDS)*sizeof(bool));
+	
+	for(i = 0; i < 5*5*NUM_OF_BOARDS; ++i)
+		checks[i] = false;
+	
+	file = fopen("input.txt", "r");
+
+    if(file == NULL)
+    {
+        fprintf(stdout, "Error opening file!\n");
+        exit(1);
+    }
+
+    // Read number order
+    i = 0;
+    while(!feof(file))
+    {
+        fscanf(file, "%hhu,", &order[i]);
+		++i;
+        if(i == INPUT_LEN)
+            break;
+    }
+	
+	// Read boards
+    i = 0;
+    while(!feof(file))
+    {
+        fscanf(file, "%hhu", &buffer[i]);
+		++i;
+    }
+	fclose(file);
+	
+	// Simulate bingo
+	for(bingo = 0; bingo < INPUT_LEN && !winner_flag; ++bingo)
+	{
+		for(i = 0; i < NUM_OF_BOARDS; ++i)
+		{
+			find_num(i*25, i*25+25, order[bingo]);
+		}
+		
+		for(i = 0; i < NUM_OF_BOARDS && !winner_flag; ++i)
+		{
+			winner_flag = check_for_bingo(i*25, i*25+25, order[bingo], i);
+		}
+	}
+	
+	printf("%d\n", sum_winner_board(winner_board) * winning_num);
+
+    return 0;
+}
+
+void find_num(u16 start, u16 end, u8 key)
+{
+	u16 i;
+	for(i = start; i < end; ++i)
+	{
+		if(buffer[i] == key)
+		{
+			checks[i] = true;
+			return;
+		}
+	}
+}
+
+bool check_for_bingo(u16 start, u16 end, u8 key, u8 board)
+{
+	u8 i;
+	
+	// check horizontal
+	for(i = 0; i < 5; ++i)
+	{
+		bool is_winner = checks[board*25 + 5*i] &&
+						 checks[board*25 + 5*i + 1] &&
+						 checks[board*25 + 5*i + 2] &&
+						 checks[board*25 + 5*i + 3] &&
+						 checks[board*25 + 5*i + 4];
+		
+		if(is_winner)
+		{
+			winner_board = board; 
+			winning_num = key;
+			return true;
+		}
+	}
+	
+	//check vertical
+	for(i = 0; i < 5; ++i)
+	{
+		bool is_winner = checks[board*25 + i] &&
+						 checks[board*25 + i + 5] &&
+						 checks[board*25 + i + 10] &&
+						 checks[board*25 + i + 15] &&
+						 checks[board*25 + i + 20];
+			
+		if(is_winner)
+		{
+			winner_board = board; 
+			winning_num = key;
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+u16 sum_winner_board(u8 board)
+{
+	u8 i, j;
+	u16 sum = 0;
+	
+	for(i = 0; i < 5; ++i)
+	{
+		for(j = 0; j < 5; ++j)
+			if(!checks[board*25 + i*5 + j])
+				sum += buffer[board*25 + i*5 + j];
+	}
+	return sum;
+}
